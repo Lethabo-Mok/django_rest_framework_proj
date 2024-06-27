9b3552ef6156d4749261b8acb53b6195b3d07b45
Lethabo-Mok <lethabo@epftechhubic.org>
2024-06-28 00:52:22 +0200

    Retrieve Update and Destroy APIViews

--- planapi/todos/urls.py
+++ planapi/todos/urls.py
@@ -1,7 +1,10 @@
-from todos.views import CreateAPIView, TodoListAPIView
+# from todos.views import CreateAPIView, TodoListAPIView
+from todos.views import TodoAPIView, TodoDetailAPIView
 from django.urls import path
 
 urlpatterns = [
-    path('create', CreateAPIView.as_view(), name="create-todo"),
-    path('list', TodoListAPIView.as_view(), name="list-todos"),
+    path("",TodoAPIView.as_view(), name="todos"),
+    # path('create', CreateAPIView.as_view(), name="create-todo"),
+    # path('list', TodoListAPIView.as_view(), name="list-todos"),
+    path("<int:id>",TodoDetailAPIView.as_view(), name="todo"),
 ]
--- planapi/todos/views.py
+++ planapi/todos/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from todos.serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
 from todos.models import Todo
 
-
-
-class CreateTodooAPIView(CreateAPIView):
+class TodoAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
     
-class TodoListAPIView(ListAPIView):
+    def get_queryset(self):
+        return Todo.objects.filter(owner=self.request.user)
+
+# class CreateTodooAPIView(CreateAPIView):
+#     serializer_class = TodoSerializer
+#     permission_classes = (IsAuthenticated,)
+
+#     def perform_create(self, serializer):
+#         return serializer.save(owner=self.request.user)
+    
+# class TodoListAPIView(ListAPIView):
+#     serializer_class = TodoSerializer
+#     permission_classes = (IsAuthenticated,)
+    
+#     def get_queryset(self):
+#         return Todo.objects.filter(owner=self.request.user)
+
+class TodoDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated,)
-    
+    lookup_field = "id"
+
     def get_queryset(self):
         return Todo.objects.filter(owner=self.request.user)
