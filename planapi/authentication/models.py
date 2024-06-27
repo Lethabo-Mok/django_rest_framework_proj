c7eb7e657c388ec311c08c41e3dfb78e02f0ec39
Lethabo-Mok <lethabo@epftechhubic.org>
2024-06-27 22:21:13 +0200

    Authenticate User

--- planapi/authentication/models.py
+++ planapi/authentication/models.py
@@ -4,11 +4,10 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.contrib.auth.models import (PermissionsMixin,UserManager, AbstractBaseUser)
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+import jwt
+from datetime import datetime
 
-# add new properties access_token, is_email_verified
-# Use email and password instead of username/password 
-
-# Create your models here.
+from django.conf import settings
 
 class MyUserManager(UserManager):
      def _create_user(self, username, email, password, **extra_fields):
@@ -101,4 +100,9 @@ class User(AbstractBaseUser, PermissionsMixin, TrackingModel):
 
     @property
     def token(self):
-         return ''
+         token = jwt.encode(
+             {'username': self.username, 'email': self.email, 
+              'exp': datetime.utcnow() + timedelta(hours=24)}, 
+              settings.SECRET_KEY, algorithm='HS256')
+         
+         return token
