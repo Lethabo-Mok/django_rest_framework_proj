db89a3296970e2baac8630f4ee516feeaf29eb1f
Lethabo-Mok <lethabo@epftechhubic.org>
2024-06-28 01:17:24 +0200

    Filtering Searching and Ordering

--- planapi/todos/views.py
+++ planapi/todos/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework import permissions, filters
 from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from todos.serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
 from todos.models import Todo
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class TodoAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['id', 'title','desc', 'is_complete']
+    search_fields = ['id', 'title','desc', 'is_complete']
+    ordering_fields = ['id', 'title','desc', 'is_complete']
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
